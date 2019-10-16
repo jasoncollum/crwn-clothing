dbb94f8847ca349606cbd5d544a3165096daa8ef
Jason Collum <jasoncollum@gmail.com>
2019-10-15 19:43:06 -0500

    add collection-preview component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,8 +50,8 @@ class Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map(({ id, ...otherSectinProps }) => (
-                        <MenuItem key={id} {...otherSectinProps} />
+                    this.state.sections.map(({ id, ...otherSectionProps }) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>
