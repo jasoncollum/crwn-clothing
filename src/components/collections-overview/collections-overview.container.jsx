c64e10788e81a7b34d804637f909301ba27c08df
Jason Collum <jasoncollum@gmail.com>
2019-11-25 14:11:11 -0600

    complete redux thunk section

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+const mapStateToProps = createStructuredSelector({
+    isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+    connect(mapStateToProps),
+    WithSpinner
+)(CollectionsOverview);
+
+// compose evaluates R to L
+
+export default CollectionsOverviewContainer;
